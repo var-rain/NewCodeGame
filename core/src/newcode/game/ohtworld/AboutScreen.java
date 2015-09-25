d8a3ee4bccd5931e67cf3a0e2d6b0838c1e2f3d7
NewCode <1934016928@outlook.com>
2015-09-25 16:57:00 +0800

    NewCodeProject

---
+++ core/src/newcode/game/ohtworld/AboutScreen.java
@@ -0,0 +1,47 @@
+package newcode.game.ohtworld;
+
+import com.badlogic.gdx.Screen;
+
+/**
+ * 项目名称: Game.
+ * 项目所属: NewCode.
+ * 创 建 人: Var "雨中行".
+ * 创建时间: 2015/9/25 16:55.
+ * 类 描 述: 关于我们的Screen
+ */
+public class AboutScreen implements Screen{
+    @Override
+    public void show() {
+
+    }
+
+    @Override
+    public void render(float delta) {
+
+    }
+
+    @Override
+    public void resize(int width, int height) {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void hide() {
+
+    }
+
+    @Override
+    public void dispose() {
+
+    }
+}
